a448087896cebcd74812cd7153b0de20e2bab176
Daniel Schischkin <daniel@schischkin.info>
2021-03-07 22:30:26 +0100

    Add middleware

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/fourhundredfour/gin_prometheus_pusher
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.6.3 // indirect
+require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/prometheus/client_golang v1.9.0 // indirect
+)
--- prometheus.go
+++ prometheus.go
@@ -1,7 +1,53 @@
 package gin_prometheus_pusher
 
-import "github.com/gin-gonic/gin"
+import (
+	"sync"
 
-func Prometheus() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/push"
+)
+
+type PrometheusConfiguration struct {
+	Job               string                  // Job Name
+	Address           string                  // Address of the Prometheus Push Gateway
+	BasicAuthUser     string                  // Http Basic Auth User
+	BasicAuthPassword string                  // Http Basic Auth Password
+	Interval          uint32                  // Interval
+	Collectors        *[]prometheus.Collector // User defined Collectors
+	Gatherers         *[]prometheus.Gatherer  // User defined Gatherers
+}
+
+func Prometheus(config PrometheusConfiguration) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		wg := &sync.WaitGroup{}
+		wg.Add(3)
+		pusher := push.New(config.Address, config.Job)
+		go configureBasicAuth(config.BasicAuthUser, config.BasicAuthPassword, pusher, wg)
+		go attachCollectors(config.Collectors, pusher, wg)
+		go attachGatherers(config.Gatherers, pusher, wg)
+		wg.Wait()
+		pusher.Add()
+	}
+}
+
+func configureBasicAuth(username string, password string, pusher *push.Pusher, wg *sync.WaitGroup) {
+	if username != "" || password != "" {
+		pusher.BasicAuth(username, password)
+	}
+	wg.Done()
+}
+
+func attachCollectors(collectors *[]prometheus.Collector, pusher *push.Pusher, wg *sync.WaitGroup) {
+	for _, collector := range *collectors {
+		pusher.Collector(collector)
+	}
+	wg.Done()
+}
+
+func attachGatherers(gatherers *[]prometheus.Gatherer, pusher *push.Pusher, wg *sync.WaitGroup) {
+	for _, gatherer := range *gatherers {
+		pusher.Gatherer(gatherer)
+	}
+	wg.Done()
 }
